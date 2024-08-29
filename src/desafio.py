da5299420b58a27bb342ede8c50d16f2a1ddf8ed
Guilherme Queiroz <guihsq1408@gmail.com>
2024-08-29 17:59:06 -0300

    criado funções menu/imprimir_extrato/main

--- src/desafio.py
+++ src/desafio.py
@@ -1,17 +1,15 @@
-menu = """
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+def menu():
+    menu = """
 
-->"""
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [q] Sair
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+    ->"""
+
+    return menu
 
 def depositar(saldo, valor, extrato,/):
     if valor > 0:
@@ -40,30 +38,40 @@ def sacar(*,saldo, valor, extrato, limite, numero_saques, LIMITE_SAQUES):
         print("Operação falhou. Informe um valor válido para saque")
 
     return saldo, extrato, numero_saques   
- 
-while True:
+
+def imprimir_extrato(saldo,/,*, extrato):
+    print("\n============== EXTRATO ==============")
+    print(extrato if extrato else "Sem registro de movimentação." )
+    print(f"Saldo: R$ {saldo:.2f}")
+    print("\n=====================================")
+
+def main():
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    LIMITE_SAQUES = 3
     
-    opcao = input(menu)
-
-    if opcao == 'd':
-        valor = float(input("Digite o valor a ser depositado: "))
-        saldo, extrato = depositar(saldo, valor, extrato)
-
-    elif opcao == 's':
-
-        valor = float(input("Digite o valor a ser sacado: "))
-        saldo, extrato, numero_saques = sacar(saldo=saldo, valor=valor, extrato=extrato, limite=limite, numero_saques=numero_saques, LIMITE_SAQUES=LIMITE_SAQUES)
-
-    elif opcao == "e":
-        print("\n============== EXTRATO ==============")
-        print(extrato if extrato else "Sem registro de movimentação." )
-        print(f"Saldo: R$ {saldo:.2f}")
-        print("\n=====================================")
+    while True:
         
-    elif opcao == "q":
-        break
+        opcao = input(menu())
 
-    else:
-        print("Operação inválida. por favor selecione novamente a operação desejada")
+        if opcao == 'd':
+            valor = float(input("Digite o valor a ser depositado: "))
+            saldo, extrato = depositar(saldo, valor, extrato)
 
+        elif opcao == 's':
 
+            valor = float(input("Digite o valor a ser sacado: "))
+            saldo, extrato, numero_saques = sacar(saldo=saldo, valor=valor, extrato=extrato, limite=limite, numero_saques=numero_saques, LIMITE_SAQUES=LIMITE_SAQUES)
+
+        elif opcao == "e":
+            imprimir_extrato(saldo, extrato=extrato)
+            
+        elif opcao == "q":
+            break
+
+        else:
+            print("Operação inválida. por favor selecione novamente a operação desejada")
+
+main()
